ce5884530e0588c2a0a97884614a8ab2e6f0c66a
afreen shaik <affushaik9493@gmail.com>
2024-09-12 21:42:13 +0530

    create the data file

---
+++ constants/Works.ts
@@ -0,0 +1,7 @@
+export const works = [
+    { title: 'Project 1', image: '/Assets/webServices.jpg' },
+    { title: 'Project 2', image: '/Assets/webServices.jpg' },
+    { title: 'Project 3', image: '/Assets/webServices.jpg' },
+    { title: 'Project 4', image: '/Assets/webServices.jpg' },
+    { title: 'Project 5', image: '/Assets/webServices.jpg' },
+  ];
