c32d09e62cde63824e050e1cd0794de6f2e94bdc
afreen shaik <affushaik9493@gmail.com>
2024-09-15 01:01:29 +0530

    divide the data into a file

--- components/ReviewSection.tsx
+++ components/ReviewSection.tsx
@@ -1,37 +1,5 @@
 import Image from 'next/image'
-
-const reviews = [
-  {
-    name: 'Alice',
-    image: '/placeholder.svg?height=48&width=48',
-    comment: 'Team approved the designs! Love it 💕.',
-    status: 'green'
-  },
-  {
-    name: 'Joshua',
-    image: '/placeholder.svg?height=48&width=48',
-    comment: 'Very responsive, when can we start the app?',
-    status: 'yellow'
-  },
-  {
-    name: 'Emmanuel',
-    image: '/placeholder.svg?height=48&width=48',
-    comment: 'Thanks mate, the video campaign just hit 1M!',
-    status: 'green'
-  },
-  {
-    name: 'Christine',
-    image: '/placeholder.svg?height=48&width=48',
-    comment: 'Never knew I could launch a product that fast...',
-    status: 'gray'
-  },
-  {
-    name: 'Joy',
-    image: '/placeholder.svg?height=48&width=48',
-    comment: 'My husband loves the logo! Thanks a bunch🙏',
-    status: 'green'
-  }
-]
+import reviews from '@/constants/Reviews'
 
 export default function ReviewsSection() {
   return (
