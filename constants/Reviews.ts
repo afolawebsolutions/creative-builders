bca0632fe5cf1cc0641b88f8e97e59b9d74f2323
afreen shaik <affushaik9493@gmail.com>
2024-09-19 01:13:48 +0530

    add correct path

--- constants/Reviews.ts
+++ constants/Reviews.ts
@@ -1,31 +1,31 @@
 const reviews = [
     {
       name: 'Alice',
-      image: '/Assets/Group 38.svg',
+      image: '/Assets/Group38.svg',
       comment: 'Team approved the designs! Love it 💕.',
       status: 'green',
     },
     {
       name: 'Joshua',
-      image: '/Assets/Group 36.svg',
+      image: '/Assets/Group36.svg',
       comment: 'Very responsive, when can we start the app?',
       status: 'yellow',
     },
     {
       name: 'Emmanuel',
-      image: '/Assets/Group 37.svg',
+      image: '/Assets/Group37.svg',
       comment: 'Thanks mate, the video campaign just hit 1M!',
       status: 'green',
     },
     {
       name: 'Christine',
-      image: '/Assets/group 35.svg',
+      image: '/Assets/Group35.svg',
       comment: 'Never knew I could launch a product that fast...',
       status: 'gray',
     },
     {
       name: 'Joy',
-      image: '/Assets/group 39.svg',
+      image: '/Assets/Group39.svg',
       comment: 'My husband loves the logo! Thanks a bunch🙏',
       status: 'green',
     },
